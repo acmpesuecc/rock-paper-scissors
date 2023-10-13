e422e9c8288e91eed1411767c3a7375b4cc99b38
NJWasTaken <114809171+NJWasTaken@users.noreply.github.com>
2023-10-14 02:35:12 +0530

    Updated code to make static log dynamic

--- rps.py
+++ rps.py
@@ -9,8 +9,13 @@ from PyQt5.QtMultimedia import QSoundEffect
 import random
 
 
+cwin = "haha I the computeri winsss!!!!!!\n"
+pwin = "you just got lucky for once- ugh fine you win\n"
+draw = "woah, its a tie\n"
 p_score_log=[]
 c_score_log=[]
+ptextlog=['','','']
+ctextlog=['','','']
 # while(True):
 #     userBull = int (input("heyy, hi! so umm\n'0' - Rock\n'1' - Paper\n'2' - Scissors\nChoose onee!! : "))
 #     compBull= random.randint(0,3)
@@ -22,6 +27,7 @@ c_score_log=[]
 #         print("haha I the computeri winsss!!!!!!\n")
 class rockpaperscissors(QMainWindow):
     def __init__(self):
+        
         super(rockpaperscissors, self).__init__()
         loadUi("rps.ui", self)
         self.setWindowTitle("ROCK-PAPER-SCISSORS")
@@ -46,6 +52,7 @@ class rockpaperscissors(QMainWindow):
         #     self.tie_sound.play()
 
     def shooted(self):
+        
         if self.rock.isChecked():
             userBull = 0
         elif self.paper.isChecked():
@@ -56,23 +63,47 @@ class rockpaperscissors(QMainWindow):
         compBull = random.randint(0, 3)
 
         if userBull == compBull:
-            self.output.setText("woah, its a tie\n")
+            self.output.setText(draw)
             c_score_log.append(1)
             p_score_log.append(1)
+            ctextlog.append(draw)
+            ptextlog.append(draw)
+            self.p1.setText(ptextlog[-1])
+            self.p2.setText(ptextlog[-2])
+            self.p3.setText(ptextlog[-3])
+            self.c1.setText(ctextlog[-1])
+            self.c2.setText(ctextlog[-2])
+            self.c3.setText(ctextlog[-3])
             # self.play_sound("tie")
         elif (
             (userBull == 0 and compBull == 2)
             or (userBull == 1 and compBull == 0)
             or (userBull == 2 and compBull == 1)
         ):
-            self.output.setText("you just got lucky for once- ugh fine you win\n")
+            self.output.setText(pwin)
             c_score_log.append(0)
             p_score_log.append(2)
+            ctextlog.append(pwin)
+            ptextlog.append(pwin)
+            self.p1.setText(ptextlog[-1])
+            self.p2.setText(ptextlog[-2])
+            self.p3.setText(ptextlog[-3])
+            self.c1.setText(ctextlog[-1])
+            self.c2.setText(ctextlog[-2])
+            self.c3.setText(ctextlog[-3])
             self.play_sound("user_win")
         else:
-            self.output.setText("haha I the computeri winsss!!!!!!\n")
+            self.output.setText(cwin)
             c_score_log.append(2)
             p_score_log.append(0)
+            ctextlog.append(cwin)
+            ptextlog.append(cwin)
+            self.p1.setText(ptextlog[-1])
+            self.p2.setText(ptextlog[-2])
+            self.p3.setText(ptextlog[-3])
+            self.c1.setText(ctextlog[-1])
+            self.c2.setText(ctextlog[-2])
+            self.c3.setText(ctextlog[-3])
             self.play_sound("computer_win")
 
         if len(c_score_log)==3:
@@ -88,6 +119,7 @@ class rockpaperscissors(QMainWindow):
                 self.output.setText("Looks like its still a draw, lets do another best of 3 again.")
                 c_score_log.clear()
                 p_score_log.clear()
+        
     
 
 app = QApplication(sys.argv)
